da26033bdce0b03befc1099d69352816ad788f4c
Debarghya Choudhury <aimsstu@gmail.com>
2026-03-28 02:01:53 +0530

    feat: add Subject model class

---
+++ src/com/attendance/model/Subject.java
@@ -0,0 +1,34 @@
+package com.attendance.model;
+
+public class Subject {
+    private String code;
+    private String name;
+    private int totalClasses;
+
+    public Subject(String code, String name) {
+        this.code = code;
+        this.name = name;
+        this.totalClasses = 0;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalClasses() {
+        return totalClasses;
+    }
+
+    public void incrementTotalClasses() {
+        this.totalClasses++;
+    }
+
+    @Override
+    public String toString() {
+        return code + " - " + name;
+    }
+}
