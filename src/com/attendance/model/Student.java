179a19859844503204b7641536abbcd7ce245eff
Debarghya Choudhury <aimsstu@gmail.com>
2026-03-28 01:53:10 +0530

    feat: add Student class extending Person (inheritance)

---
+++ src/com/attendance/model/Student.java
@@ -0,0 +1,25 @@
+package com.attendance.model;
+
+public class Student extends Person {
+    private String rollNumber;
+    private String department;
+
+    public Student(String id, String name, String rollNumber, String department) {
+        super(id, name);
+        this.rollNumber = rollNumber;
+        this.department = department;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Student[" + getRollNumber() + "] " + getName() + " - " + department;
+    }
+}
